c0cb06725002edb4b67d9750eb66016d036dbdcd
Kevin RRivera <krivera1015@gmail.com>
2018-10-20 16:44:44 -0400

    Working with spinning Roulette, trying to get my ctx globally

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,12 +3,14 @@ import LocationContainer from './LocationContainer'
 import RestaurantContainer from './RestaurantContainer';
 import HomePage from '../components/HomePage'
 import { Route, Switch } from 'react-router-dom'
+import RouletteWheel from '../components/RouletteWheel';
 
 class MainContainer extends Component {
 
     render () {
         return (
             <div className='App'>
+                <RouletteWheel/>
                 <Switch>
                     <Route path='/homepage'><HomePage/></Route>
                     <Route path='/location'><LocationContainer/></Route>
