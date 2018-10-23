452c15244fc9401becd8a0d7fcb38fedeb1d75e7
Kevin RRivera <krivera1015@gmail.com>
2018-10-23 15:48:51 -0400

    Fixing code to make wheel spin

--- src/components/RouletteWheel.js
+++ src/components/RouletteWheel.js
@@ -14,7 +14,7 @@ class RouletteWheel extends Component {
             spinTimeout: null,
             spinArcStart: 10,
             spinAngleStart: 0,
-            spinTime: 0,
+            spinTime: 30,
             spinTimeTotal: 0,
             outsideRadius: 200,
             textRadius: 160,
@@ -31,48 +31,42 @@ class RouletteWheel extends Component {
         })
     }
 
-    spin = () => {
-        console.log("spin function")
-        this.setState({
-                //will start the spin at random spot
-                spinAngleStart: Math.random() * 10 + 10,
-                //this is where the spin time starts at
-                spinTime: 0,
-                //this is how long the wheel will spin
-                spinTimeTotal: Math.random() * 3 + 4 * 1000
-            },
-            () => this.rotateWheel())
-    }
+    // spin = () => {
+    //     console.log("spin function hit")
+    //     this.setState({
+    //             //will start the spin at random spot
+    //             spinAngleStart: Math.random() * 10 + 10,
+    //             //this is where the spin time starts at
+    //             spinTime: 0,
+    //             //this is how long the wheel will spin
+    //             spinTimeTotal: Math.random() * 3 + 4 * 1000
+    //         },
+    //         //() => this.rotateWheel()
+    //         )
+    // }
 
-    rotateWheel = () => {
-        while (this.state.spinTime < this.state.spinTimeTotal) {
-            this.setState({
-                spinTime: this.state.spinTime + 30
-            })
-            if (this.state.spinTime <= this.state.spinTimeTotal) {
-                console.log("Im spinning", this.state.spinTimeTotal, this.state.spinTime)
-                this.stopRotateWheel();
-                //return;
-            }
-        }
-            let spinAngle = this.state.spinAngleStart - this.easeOut(this.state.spinTime, 0, this.state.spinAngleStart, this.state.spinTimeTotal);
-            this.setState({startAngle: this.state.startAngle + (spinAngle * Math.PI / 180)})
-            this.drawRouletteWheel();
-            this.setState({spinTimeout: this.setTimeout('rotateWheel()', 30)});
-        console.log("after", this.state.spinTime)
-    }
+    // rotateWheel = () => {
+    //     while (this.state.spinTime < this.state.spinTimeTotal) {
+    //         this.setState({
 
-    stopRotateWheel = () => {
-        console.log("stopppped bro")
-        clearTimeout(this.state.spinTimeout);
-        let degrees = this.state.startAngle * 180 / Math.PI + 90;
-        let arcd = this.state.arc * 180 / Math.PI;
-        let index = Math.floor((360 - degrees % 360) / arcd);
-        this.state.canvasContext.save();
-        let text = options[index]
-        this.state.canvasContext.fillText(text, 250 - this.state.canvasContext.measureText(text).width / 2, 250 + 10);
-        this.state.canvasContext.restore();
-    }
+    //         })
+    //     }
+    //         let spinAngle = this.state.spinAngleStart - this.easeOut(this.state.spinTime, 0, this.state.spinAngleStart, this.state.spinTimeTotal)
+    //         this.setState({startAngle: this.state.startAngle + (spinAngle * Math.PI / 180)})
+    //         this.drawRouletteWheel(this.state.canvasContext)
+    // }
+
+    // stopRotateWheel = () => {
+    //     console.log("stopppped bro")
+    //     clearTimeout(this.state.spinTimeout)
+    //     let degrees = this.state.startAngle * 180 / Math.PI + 90
+    //     let arcd = this.state.arc * 180 / Math.PI
+    //     let index = Math.floor((360 - degrees % 360) / arcd)
+    //     this.state.canvasContext.save()
+    //     let text = options[index]
+    //     this.state.canvasContext.fillText(text, 250 - this.state.canvasContext.measureText(text).width / 2, 250 + 10)
+    //     this.state.canvasContext.restore()
+    // }
 
     drawRouletteWheel = (canvas) => {
         //will clear the whole canvas
@@ -118,11 +112,11 @@ class RouletteWheel extends Component {
         canvas.fill();
 }
 
-easeOut = (t, b, c, d) => {
-  let ts = (t/=d)*t;
-  let tc = ts*t;
-  return b+c*(tc + -3*ts + 3*t);
-}
+// easeOut = (t, b, c, d) => {
+//   let ts = (t/=d)*t;
+//   let tc = ts*t;
+//   return b+c*(tc + -3*ts + 3*t);
+// }
 
     render() {
         //console.log("RoulletteWheel's canvas context:",this.state.canvasContext)
@@ -139,42 +133,3 @@ easeOut = (t, b, c, d) => {
 }
 
 export default RouletteWheel
-
-// function spin() {
-//   spinAngleStart = Math.random() * 10 + 10;
-//   spinTime = 0;
-//   spinTimeTotal = Math.random() * 3 + 4 * 1000;
-//   rotateWheel();
-// }
-
-// function rotateWheel() {
-//   spinTime += 30;
-//   if(spinTime >= spinTimeTotal) {
-//     stopRotateWheel();
-//     return;
-//   }
-//   var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-//   startAngle += (spinAngle * Math.PI / 180);
-//   drawRouletteWheel();
-//   spinTimeout = setTimeout('rotateWheel()', 30);
-// }
-
-// function stopRotateWheel() {
-//   clearTimeout(spinTimeout);
-//   var degrees = startAngle * 180 / Math.PI + 90;
-//   var arcd = arc * 180 / Math.PI;
-//   var index = Math.floor((360 - degrees % 360) / arcd);
-//  canvas.save();
-//  canvas.font = 'bold 30px Helvetica, Arial';
-//   var text = options[index]
-//  canvas.fillText(text, 250 -canvas.measureText(text).width / 2, 250 + 10);
-//  canvas.restore();
-// }
-
-// function easeOut(t, b, c, d) {
-//   var ts = (t/=d)*t;
-//   var tc = ts*t;
-//   return b+c*(tc + -3*ts + 3*t);
-// }
-
-// drawRouletteWheel();
